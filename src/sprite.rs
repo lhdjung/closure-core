0728bf773f5a7b1c97e5c49a6889577f046139ff
nrposner <nicolasposner@gmail.com>
2025-04-15 16:12:59 -0500

    added helper fn for sequence generation

--- src/sprite.rs
+++ src/sprite.rs
@@ -1,3 +1,9 @@
 //! Creating a space to experiment with a Rust translation of SPRITE
 
 
+fn generate_sequence(min_val: i32, max_val: i32, n_items: u32, i: u32) -> Vec<f64> {
+    let increment = (1.0 / n_items as f64) * (i as f64 - 1.0);
+    (min_val..max_val)
+        .map(|val| val as f64 + increment)
+        .collect()
+}
