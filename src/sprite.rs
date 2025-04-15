767b73f2f0b7fd302a9c4d15a4862a804447ec54
nrposner <nicolasposner@gmail.com>
2025-04-15 16:13:22 -0500

    added helper function for the abm section

--- src/sprite.rs
+++ src/sprite.rs
@@ -1,5 +1,35 @@
 //! Creating a space to experiment with a Rust translation of SPRITE
 
+fn abm_internal(total: f64, n_obs: f64, a: f64, b: f64) -> Vec<f64>{
+
+    let k1 = rust_round((total - (n_obs * b)) / (a - b), 0);
+    let k = f64::min(f64::max(k1, 1.0), n_obs - 1.0);
+
+    let mut v1 = vec![a; k.floor() as usize];
+    let mut v2 = vec![b; (n_obs - k).floor() as usize];
+
+    v2.append(&mut v1);
+
+    let diff = v2.iter().sum::<f64>() - total;
+
+    // assuming we're cmpletely overwriting the contents of the vector we made before, so I've
+    // disambiguated the variable names
+    //
+
+    let combined_vec: Vec<f64> = match diff < 0.0 {
+        true => vec![a; (k - 1.0).floor() as usize]
+            .into_iter()
+            .chain(once(a + diff.abs()))
+            .chain(vec![b; (n_obs - k).floor() as usize])
+            .collect(),
+        false => vec![a; k as usize]
+            .into_iter()
+            .chain(once(b - diff))
+            .chain(vec![b; (n_obs - k - 1.0).floor() as usize]).collect(),
+    };
+
+    combined_vec
+}
 
 fn generate_sequence(min_val: i32, max_val: i32, n_items: u32, i: u32) -> Vec<f64> {
     let increment = (1.0 / n_items as f64) * (i as f64 - 1.0);
