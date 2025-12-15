26d8e416dc043eb4fb038581ab7b24bb4f633c0e
Lukas Jung <jung-lukas@gmx.net>
2025-12-15 10:51:34 +0100

    Use new `sprite_parallel()` in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,31 @@
-pub mod grimmer;
-pub mod sprite;
-pub mod sprite_types;
-
-use crate::sprite::{find_possible_distributions, set_parameters};
-use crate::sprite_types::RestrictionsOption;
+use closure_core::sprite::{mean, sprite_parallel};
+use closure_core::sprite_types::RestrictionsOption;
 use rand::prelude::*;
 use rand::rngs::StdRng;
 
 fn main() {
-    let sprite_parameters = set_parameters(
-        2.2,
-        1.3,
+    let mut rng = StdRng::seed_from_u64(1234);
+
+    let results: Vec<Vec<i32>> = sprite_parallel(
+        2.2_f64,
+        1.3_f64,
         23,
         1,
         5,
         None,
         None,
         1,
+        5,
         None,
         RestrictionsOption::Default,
         false,
+        &mut rng,
     )
     .unwrap();
 
-    let results = find_possible_distributions(
-        &sprite_parameters,
-        5,
-        false,
-        &mut StdRng::seed_from_u64(1234),
-    );
+    // Convert scaled integers to floats (scale factor is 10 for precision 1)
+    let first_dist: Vec<f64> = results[0].iter().map(|&v| v as f64 / 10.0).collect();
+    let computed_mean = mean(&first_dist);
 
-    assert_eq!(results[0].mean, 2.2);
+    assert_eq!((computed_mean * 10.0).round() / 10.0, 2.2);
 }
